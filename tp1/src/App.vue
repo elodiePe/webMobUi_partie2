823c840ab508e4241d5fc26ef50b68e8768d979e
Elodie Perring <elodie.perring@heig-vd-ch>
2024-04-09 11:48:47 +0200

    TP1 température ans App.vue

--- tp1/src/App.vue
+++ tp1/src/App.vue
@@ -1,30 +1,51 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <!-- Champs de saisie pour Kelvin, Celsius et Fahrenheit -->
+    <input type="number" :value="tempKelvin" @input="tempKelvin = $event.target.value">
+    <input type="number" v-model="tempCelsius" placeholder="Celsius">
+    <input type="number" v-model="tempFaranheit" placeholder="Fahrenheit">
+<!-- <input type="number" v-model="tempSi"> Si on utilise v-model au lieu de :value, il n'y a pas besoin d'appeler un event UNIQUEMENT si la valeur tempSi est une ref. V-model fait automatiquement le 2-way binding -->
+    <!-- Affichage des messages -->
+    <p v-if="tempKelvin < 0">La température en Kelvin est négative</p>
+    <p v-else-if="tempKelvin === 0">La température est nulle</p>
+    <p v-else>La température est positive</p>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup>
+import { computed, ref, watch } from 'vue';
+
+const tempSi = ref(0);
+
+const tempKelvin = computed({
+  get: () => tempSi.value,
+  set: (value) => {
+    tempSi.value = value;
+  }
+});
+
+const tempCelsius = computed({
+  get: () => tempSi.value - 273.15,
+  set: (value) => {
+    tempSi.value = value + 273.15;
+  }
+});
+
+const tempFaranheit = computed({
+  get: () => tempSi.value * 9/5 - 459.67,
+  set: (value) => {
+    tempSi.value = value + 459.67 /9/5;
+  }
+});
+watch(tempSi, (newtempSi, oldtempSi) => {
+  console.log(`Kelvin: ${newtempSi}, Celsius: ${tempCelsius.value}, Fahrenheit: ${tempFaranheit.value}`);
+  console.log(`Ancienne valeur : ${oldtempSi}`);
+});
+</script>
+
+<style>
+body {
+  background-color: rgb(214, 160, 160);
 }
 </style>
